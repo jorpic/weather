820e8db945ccf324ddea47d598ea93f1ceaab9d9
Max Taldykin <jorpic@gmail.com>
2024-03-02 22:48:46 +0400

    station: add BMP180 sensor (temperature & pressure)

--- station/src/main.rs
+++ station/src/main.rs
@@ -8,7 +8,9 @@ use rtt_target::{rprintln, rtt_init_print};
 use nb::block;
 
 use cortex_m_rt::entry;
-use stm32f1xx_hal::{prelude::*, pac, timer::Timer};
+use stm32f1xx_hal::{prelude::*, pac, i2c, timer::Timer};
+
+use embedded_drivers::bmp180::BMP180;
 
 #[entry]
 fn main() -> ! {
@@ -19,33 +21,55 @@ fn main() -> ! {
     let mut core = cortex_m::Peripherals::take().unwrap();
     let pac = pac::Peripherals::take().unwrap();
 
-    {   // enable debug cycle counter
-        core.DCB.enable_trace();
+    {   // enable debug cycle counter (required by I2C)
         core.DWT.enable_cycle_counter();
+        // FIXME: ??
+        core.DCB.enable_trace();
     }
 
     let mut flash = pac.FLASH.constrain();
+    let mut afio = pac.AFIO.constrain();
     let rcc = pac.RCC.constrain();
 
     // Setup ADC clock
     let clocks = rcc.cfgr.adcclk(2.MHz()).freeze(&mut flash.acr);
 
+    let mut delay = pac.TIM1.delay_ms(&clocks);
+
     // Configure the syst timer to trigger an update every second
     let mut timer = Timer::syst(core.SYST, &clocks).counter_hz();
     timer.start(1.Hz()).unwrap();
 
+    let mut gpioa = pac.GPIOA.split();
     let mut gpiob = pac.GPIOB.split();
-    let mut gpioc = pac.GPIOC.split();
 
-    // Btn and LED
-    let _btn = gpiob.pb12.into_pull_up_input(&mut gpiob.crh);
-    let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
+    // I2C
+    let i2c = {
+        let scl = gpiob.pb6.into_alternate_open_drain(&mut gpiob.crl);
+        let sda = gpiob.pb7.into_alternate_open_drain(&mut gpiob.crl);
+
+        i2c::BlockingI2c::i2c1(
+            pac.I2C1,
+            (scl, sda),
+            &mut afio.mapr,
+            i2c::Mode::Fast {
+                frequency: 100.kHz(),
+                duty_cycle: i2c::DutyCycle::Ratio16to9,
+            },
+            clocks,
+            1000, // start_timeout_us
+            10,   // start_retries
+            1000, // addr_timeout_us
+            1000, // data_timeout_us
+        )
+    };
+
+    let mut bmp180 = BMP180::new(i2c);
+    bmp180.init();
 
     loop {
-        led.set_low();
-        let time = pac::DWT::cycle_count();
-        rprintln!("{}", time);
+        rprintln!("t: {}", bmp180.get_temperature(&mut delay));
+        rprintln!("p: {}", bmp180.get_pressure(&mut delay));
         block!(timer.wait()).unwrap();
-        led.set_high();
     }
 }
