9f213473c53de09d556bfa50fb02699ee9ca6fed
Max Taldykin <jorpic@gmail.com>
2024-03-21 14:51:58 +0400

    station: drop USB support

--- station/src/main.rs
+++ station/src/main.rs
@@ -8,10 +8,7 @@ use rtt_target::{rprintln, rtt_init_print};
 use core::sync::atomic::{AtomicBool, Ordering};
 
 use cortex_m_rt::entry;
-use stm32f1xx_hal::{prelude::*, pac, i2c, usb};
-
-use usb_device::device::{UsbDeviceBuilder, UsbVidPid};
-use usbd_serial::{SerialPort, USB_CLASS_CDC};
+use stm32f1xx_hal::{prelude::*, pac, i2c};
 
 use embedded_drivers::bmp180::BMP180;
 use lsm303dlhc::Lsm303dlhc;
@@ -52,24 +49,6 @@ fn main() -> ! {
     // LED
     let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
 
-    // USB CDC-ACM
-    assert!(clocks.usbclk_valid());
-
-    let usb_bus = usb::UsbBus::new(usb::Peripheral {
-        usb: pac.USB,
-        pin_dm: gpioa.pa11,
-        pin_dp: gpioa.pa12,
-    });
-
-    let mut usb_serial = SerialPort::new(&usb_bus);
-    let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x16c0, 0x27dd))
-        .manufacturer("Fake company")
-        .product("Weather station")
-        .serial_number("v0.0")
-        .device_class(USB_CLASS_CDC)
-        .self_powered(true)
-        .build();
-
     // I2C
     let i2c = {
         let scl = gpiob.pb6.into_alternate_open_drain(&mut gpiob.crl);
@@ -96,8 +75,6 @@ fn main() -> ! {
 
     let mut loop_count = 0usize;
     loop {
-        usb_dev.poll(&mut [&mut usb_serial]);
-
         if !LOOP_FLAG.swap(false, Ordering::Relaxed) {
             continue;
         }
@@ -112,7 +89,6 @@ fn main() -> ! {
         rprintln!("t: {}", bmp180.get_temperature(&mut delay));
         rprintln!("p: {}", bmp180.get_pressure(&mut delay));
 
-        let _ = usb_serial.write(b"hello world!\r\n");
         led.set_low();
         delay.delay_ms(500u16);
         led.set_high();
